b8bc4eb3ae274f998ae729d50139c8e6abe312d4
CaffeineOnIce <samanwaya.dutta@gmail.com>
2024-01-03 18:04:55 +0530

    clearing vulnerabilities

--- extension.js
+++ extension.js
@@ -14,8 +14,8 @@ let enableMinFileHtml = true;
 let enableMinFileCss = true;
 let enableMinFileJs = true;
 let enableSourceMaps = false;
-let enableSeparateFolderHtml = false; 
-let enableSeparateFolderCss = false; 
+let enableSeparateFolderHtml = false;
+let enableSeparateFolderCss = false;
 let enableSeparateFolderJs = false;
 
 
@@ -26,22 +26,22 @@ function activate(context) {
 		const { languageId, fileName } = document;
 		var parentPath = path.parse(fileName).dir;
 		const inputFile = document.getText();
-		
-		if (onlyParent.length>0 && onlyParent.split(",").filter(f=>parentPath.includes(f)).length===0) {
+
+		if (onlyParent.length > 0 && onlyParent.split(",").filter(f => parentPath.includes(f)).length === 0) {
 			return;
-		} else if (onlyParent.length>0 && rootChangeRootName===true) {
-		    var root = onlyParent.split(",").filter(f=>parentPath.includes(f))[0];
+		} else if (onlyParent.length > 0 && rootChangeRootName === true) {
+			var root = onlyParent.split(",").filter(f => parentPath.includes(f))[0];
 			if (root) {
 				var rootPath = parentPath.replace(new RegExp(root + "(.*$)", "gi"), root),
-				    rootMin = path.join(rootPath + "\\min"),
-				    structure = parentPath.replace(rootPath, "").replace(/^\\/, "");
+					rootMin = path.join(rootPath + "\\min"),
+					structure = parentPath.replace(rootPath, "").replace(/^\\/, "");
 				parentPath = path.join(rootPath + "\\min\\" + structure);
 
 				if (!fs.existsSync(rootMin)) {
 					fs.mkdirSync(rootMin);
 				}
 
-				structure.split("\\").forEach(s=>{
+				structure.split("\\").forEach(s => {
 					rootMin = path.join(rootMin + "\\" + s);
 					if (!fs.existsSync(rootMin)) {
 						fs.mkdirSync(rootMin);
@@ -49,7 +49,7 @@ function activate(context) {
 				});
 			}
 		}
-				
+
 		if (!shouldMinify(languageId)) {
 			return;
 		}
@@ -77,28 +77,28 @@ function activate(context) {
 					minifyJS: true
 				});
 				await writeFile(outputFile, minifiedHTML);
-				console.clear(); 
+				console.clear();
 				vscode.window.setStatusBarMessage('HTML Minified', 2000);
 				break;
 
 			case 'css':
 				outputFile = getOutputFilePath(parentPath, fileName, '.css', enableSeparateFolderCss);
-				let minifiedCSS = new minifyCSS({ 
-					level: { 
-						1: { 
-							all: true 
-						} 
-					} 
+				let minifiedCSS = new minifyCSS({
+					level: {
+						1: {
+							all: true
+						}
+					}
 				}).minify(inputFile);
 				await writeFile(outputFile, minifiedCSS.styles);
-				console.clear(); 
+				console.clear();
 				vscode.window.setStatusBarMessage('CSS Minified', 2000);
 				break;
 
 			case 'javascript':
 				outputFile = getOutputFilePath(parentPath, fileName, '.js', enableSeparateFolderJs);
 				sourceMapFile = `${outputFile}.map`;
-				let options = { 
+				let options = {
 					mangle: false,
 					sourceMap: enableSourceMaps ? {
 						url: path.basename(sourceMapFile)
@@ -107,16 +107,16 @@ function activate(context) {
 				let minifiedJS = await minify(inputFile, options);
 				await writeFile(outputFile, minifiedJS.code);
 				if (enableSourceMaps) {
-					await writeFile(sourceMapFile, minifiedJS.map); 
+					await writeFile(sourceMapFile, minifiedJS.map);
 				}
-				console.clear(); 
+				console.clear();
 				vscode.window.setStatusBarMessage('JS Minified', 2000);
 				break;
 		}
 	});
 
 	context.subscriptions.push(vscode.commands.registerCommand('autominify.minify', () => {
-		
+
 	}));
 
 	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration((event) => {
@@ -139,7 +139,7 @@ function activate(context) {
 	}));
 }
 
-function deactivate() {}
+function deactivate() { }
 
 function updateSettings() {
 	const config = vscode.workspace.getConfiguration('autominify');
@@ -151,10 +151,10 @@ function updateSettings() {
 	enableMinFileHtml = config.get('enableMinFileHtml', true);
 	enableMinFileCss = config.get('enableMinFileCss', true);
 	enableMinFileJs = config.get('enableMinFileJs', true);
-    enableSourceMaps = config.get('enableSourceMaps', false);
-    enableSeparateFolderHtml = config.get('enableSeparateFolderHtml', false);
-    enableSeparateFolderCss = config.get('enableSeparateFolderCss', false);
-    enableSeparateFolderJs = config.get('enableSeparateFolderJs', false);
+	enableSourceMaps = config.get('enableSourceMaps', false);
+	enableSeparateFolderHtml = config.get('enableSeparateFolderHtml', false);
+	enableSeparateFolderCss = config.get('enableSeparateFolderCss', false);
+	enableSeparateFolderJs = config.get('enableSeparateFolderJs', false);
 }
 
 function shouldMinify(languageId) {
@@ -208,17 +208,17 @@ async function writeFile(outputPath, content) {
 }
 
 function getOutputFilePath(parentPath, fileName, extension, enableSeparateFolder) {
-    const outputPathWithoutExtension = path.join(parentPath, `${path.parse(fileName).name}`+(!rootChangeRootName? `.min` : ""));
-    console.log(outputPathWithoutExtension);
-    if (enableSeparateFolder) {
-        const minFolder = path.join(parentPath, 'min');
-        if (!fs.existsSync(minFolder)) {
-            fs.mkdirSync(minFolder);
-        }
-        return path.join(minFolder, `${path.parse(fileName).name}`+(!rootChangeRootName? `.min` : "")+`${extension}`);
-    } else {
-        return `${outputPathWithoutExtension}${extension}`;
-    }
+	const outputPathWithoutExtension = path.join(parentPath, `${path.parse(fileName).name}` + (!rootChangeRootName ? `.min` : ""));
+	console.log(outputPathWithoutExtension);
+	if (enableSeparateFolder) {
+		const minFolder = path.join(parentPath, 'min');
+		if (!fs.existsSync(minFolder)) {
+			fs.mkdirSync(minFolder);
+		}
+		return path.join(minFolder, `${path.parse(fileName).name}` + (!rootChangeRootName ? `.min` : "") + `${extension}`);
+	} else {
+		return `${outputPathWithoutExtension}${extension}`;
+	}
 }
 
 module.exports = {
