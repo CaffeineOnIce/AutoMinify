bf7bea7f6e32b9280cbf228b52fc66f7097f8484
CaffeineOnIce <samanwaya.dutta@gmail.com>
2024-01-04 21:30:48 +0530

    simplified the code

--- extension.js
+++ extension.js
@@ -1,137 +1,129 @@
 const vscode = require('vscode');
-const { minify } = require('terser');
-const minifyCSS = require('clean-css');
-const minifyHTML = require('html-minifier').minify;
 const fs = require('fs');
+const { promises: fsPromises } = require('fs');
 const path = require('path');
 
-let onlyParent = "";
-let changeRootName = false;
-let enableHtmlMinification = true;
-let enableCssMinification = true;
-let enableJsMinification = true;
-let enableMinFileHtml = true;
-let enableMinFileCss = true;
-let enableMinFileJs = true;
-let enableSourceMaps = false;
-let enableSeparateFolderHtml = false;
-let enableSeparateFolderCss = false;
-let enableSeparateFolderJs = false;
+const { minify: minifyHTML } = require('html-minifier-terser');
+const { minify: minifyJS } = require('terser');
+const minifyCSS = require('clean-css');
+
+let enableHTMLMinification = true;
+let enableJSMinification = true;
+let enableCSSMinification = true;
+
+let enableSeparateFolderHTML = false;
+let enableSeparateFolderJS = false;
+let enableSeparateFolderCSS = false;
+
+let enableShowInPreviewOnly = false;
+
+let currentPanel;
 
 function activate(context) {
 	updateSettings();
 
 	vscode.workspace.onDidSaveTextDocument(async (document) => {
 		const { languageId, fileName } = document;
-		var parentPath = path.parse(fileName).dir;
+		const parentPath = path.parse(fileName).dir;
 		const inputFile = document.getText();
+		let outputFile;
 
-		if (onlyParent.length > 0 && onlyParent.split(",").filter(f => parentPath.includes(f)).length === 0) {
+		if (shouldSkipMinification(fileName)) {
 			return;
-		} else if (onlyParent.length > 0 && changeRootName === true) {
-			var root = onlyParent.split(",").filter(f => parentPath.includes(f))[0];
-			if (root) {
-				var rootPath = parentPath.replace(new RegExp(root + "(.*$)", "gi"), root),
-					rootMin = path.join(rootPath + "\\min"),
-					structure = parentPath.replace(rootPath, "").replace(/^\\/, "");
-				parentPath = path.join(rootPath + "\\min\\" + structure);
-
-				if (!fs.existsSync(rootMin)) {
-					fs.mkdirSync(rootMin);
-				}
-
-				structure.split("\\").forEach(s => {
-					rootMin = path.join(rootMin + "\\" + s);
-					if (!fs.existsSync(rootMin)) {
-						fs.mkdirSync(rootMin);
-					}
-				});
-			}
 		}
 
 		if (!shouldMinify(languageId)) {
 			return;
 		}
 
-		if (isMinFile(fileName) && !shouldMinifyMinFiles(languageId)) {
-			return;
-		}
-
-		let outputFile;
-		let sourceMapFile;
-
 		switch (languageId) {
-			case 'html':
-				outputFile = getOutputFilePath(parentPath, fileName, '.html', enableSeparateFolderHtml);
 
-				let minifiedHTML = minifyHTML(inputFile, {
-					caseSensitive: true,
-					collapseWhitespace: true,
-					collapseInlineTagWhitespace: true,
-					continueOnParseError: true,
-					removeComments: true,
+			case 'html':
+				outputFile = getOutputFilePath(parentPath, fileName, '.html', enableSeparateFolderHTML);
+
+				let minifiedHTML = await minifyHTML(inputFile, {
 					removeAttributeQuotes: true,
+					removeComments: true,
+					removeEmptyElements: true,
+					removeOptionalTags: true,
 					removeRedundantAttributes: true,
+
+					collapseWhitespace: true,
+					conservativeCollapse: true,
+
+					caseSensitive: true,
+					continueOnParseError: true,
+					collapseBooleanAttributes: true,
+					processConditionalComments: true,
+
 					minifyCSS: true,
-					minifyJS: true
+					minifyJS: true,
+					html5: true
 				});
-				await writeFile(outputFile, minifiedHTML);
-				console.clear();
+
+				if (enableShowInPreviewOnly) {
+					openWebviewPanel(minifiedHTML);
+				} else {
+					await writeFile(outputFile, minifiedHTML);
+				}
+
 				vscode.window.setStatusBarMessage('HTML Minified', 2000);
 				break;
 
+			case 'javascript':
+				outputFile = getOutputFilePath(parentPath, fileName, '.js', enableSeparateFolderJS);
+
+				let options = {
+					mangle: false
+				};
+
+				let minifiedJS = await minifyJS(inputFile, options);
+
+
+				if (enableShowInPreviewOnly) {
+					openWebviewPanel(minifiedJS.code);
+				} else {
+					await writeFile(outputFile, minifiedJS.code);
+				}
+
+				vscode.window.setStatusBarMessage('JS Minified', 2000);
+				break;
+
+
 			case 'css':
-				outputFile = getOutputFilePath(parentPath, fileName, '.css', enableSeparateFolderCss);
-				let minifiedCSS = new minifyCSS({
+				outputFile = getOutputFilePath(parentPath, fileName, '.css', enableSeparateFolderCSS);
+
+				var minifiedCSS = new minifyCSS({
 					level: {
 						1: {
 							all: true
 						}
 					}
 				}).minify(inputFile);
-				await writeFile(outputFile, minifiedCSS.styles);
-				console.clear();
-				vscode.window.setStatusBarMessage('CSS Minified', 2000);
-				break;
 
-			case 'javascript':
-				outputFile = getOutputFilePath(parentPath, fileName, '.js', enableSeparateFolderJs);
-				sourceMapFile = `${outputFile}.map`;
-				let options = {
-					mangle: false,
-					sourceMap: enableSourceMaps ? {
-						url: path.basename(sourceMapFile)
-					} : false
-				};
-				let minifiedJS = await minify(inputFile, options);
-				await writeFile(outputFile, minifiedJS.code);
-				if (enableSourceMaps) {
-					await writeFile(sourceMapFile, minifiedJS.map);
+				if (enableShowInPreviewOnly) {
+					openWebviewPanel(minifiedCSS.styles);
+				} else {
+					await writeFile(outputFile, minifiedCSS.styles);
 				}
-				console.clear();
-				vscode.window.setStatusBarMessage('JS Minified', 2000);
+
+				vscode.window.setStatusBarMessage('CSS Minified', 2000);
 				break;
 		}
 	});
 
-	context.subscriptions.push(vscode.commands.registerCommand('autominify.minify', () => {
-	}));
-
 	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration((event) => {
 		if (
-			event.affectsConfiguration('autominify.rootPathName') ||
-			event.affectsConfiguration('autominify.changeRootName') ||
-			event.affectsConfiguration('autominify.enableHtmlMinification') ||
-			event.affectsConfiguration('autominify.enableCssMinification') ||
-			event.affectsConfiguration('autominify.enableJsMinification') ||
-			event.affectsConfiguration('autominify.enableMinFileHtml') ||
-			event.affectsConfiguration('autominify.enableMinFileCss') ||
-			event.affectsConfiguration('autominify.enableMinFileJs') ||
-			event.affectsConfiguration('autominify.enableSourceMaps') ||
-			event.affectsConfiguration('autominify.enableSeparateFolderHtml') ||
-			event.affectsConfiguration('autominify.enableSeparateFolderCss') ||
-			event.affectsConfiguration('autominify.enableSeparateFolderJs')
-		) {
+			event.affectsConfiguration('autominify.enableHTMLMinification') ||
+			event.affectsConfiguration('autominify.enableJSMinification') ||
+			event.affectsConfiguration('autominify.enableCSSMinification') ||
+
+			event.affectsConfiguration('autominify.enableSeparateFolderHTML') ||
+			event.affectsConfiguration('autominify.enableSeparateFolderJS') ||
+			event.affectsConfiguration('autominify.enableSeparateFolderCSS') ||
+
+			event.affectsConfiguration('autominify.enableShowInPreviewOnly')
+		){
 			updateSettings();
 		}
 	}));
@@ -141,81 +133,100 @@ function deactivate() { }
 
 function updateSettings() {
 	const config = vscode.workspace.getConfiguration('autominify');
-	onlyParent = config.get('rootPathName', "");
-	changeRootName = config.get('changeRootName', false);
-	enableHtmlMinification = config.get('enableHtmlMinification', true);
-	enableCssMinification = config.get('enableCssMinification', true);
-	enableJsMinification = config.get('enableJsMinification', true);
-	enableMinFileHtml = config.get('enableMinFileHtml', true);
-	enableMinFileCss = config.get('enableMinFileCss', true);
-	enableMinFileJs = config.get('enableMinFileJs', true);
-	enableSourceMaps = config.get('enableSourceMaps', false);
-	enableSeparateFolderHtml = config.get('enableSeparateFolderHtml', false);
-	enableSeparateFolderCss = config.get('enableSeparateFolderCss', false);
-	enableSeparateFolderJs = config.get('enableSeparateFolderJs', false);
+	enableShowInPreviewOnly = config.get('enableShowInPreviewOnly', false);
+
+	enableHTMLMinification = config.get('enableHTMLMinification', true);
+	enableJSMinification = config.get('enableJSMinification', true);
+	enableCSSMinification = config.get('enableCSSMinification', true);
+
+	enableSeparateFolderHTML = config.get('enableSeparateFolderHTML', false);
+	enableSeparateFolderJS = config.get('enableSeparateFolderJS', false);
+	enableSeparateFolderCSS = config.get('enableSeparateFolderCSS', false);
+
 }
 
+// preview pane 
+function openWebviewPanel(outputContent) {
+	const htmlContent = `
+		<style>
+			pre {
+				white-space: pre-wrap;
+				word-wrap: break-word;
+			}
+		</style>
+		<pre>${escapeHtml(outputContent)}</pre>
+	`;
+
+	if (currentPanel) {
+		currentPanel.webview.html = htmlContent;
+	} else {
+		currentPanel = vscode.window.createWebviewPanel(
+			'codePreview',
+			'Minified Code Preview',
+			vscode.ViewColumn.Two,
+			{
+				enableScripts: true
+			}
+		);
+
+		currentPanel.webview.html = htmlContent;
+
+		currentPanel.onDidDispose(() => {
+			currentPanel = undefined;
+		});
+	}
+}
+
+// preventing '<' or 'less than' from being interpreted as actual HTML when rendered in preview pane
+function escapeHtml(unsafe) {
+	return unsafe.replace(/</g, '&lt;');
+}
+
+// check if the language id matches or not
 function shouldMinify(languageId) {
 	switch (languageId) {
 		case 'html':
-			return enableHtmlMinification;
-
-		case 'css':
-			return enableCssMinification;
+			return enableHTMLMinification;
 
 		case 'javascript':
-			return enableJsMinification;
+			return enableJSMinification;
+
+		case 'css':
+			return enableCSSMinification;
 
 		default:
 			return false;
 	}
 }
 
-function shouldMinifyMinFiles(languageId) {
-	switch (languageId) {
-		case 'html':
-			return enableMinFileHtml;
-
-		case 'css':
-			return enableMinFileCss;
-
-		case 'javascript':
-			return enableMinFileJs;
-
-		default:
-			return false;
-	}
+// skipping minification for min named files
+function shouldSkipMinification(fileName) {
+	return fileName.includes('.min.html') || fileName.includes('.min.js') || fileName.includes('.min.css');
 }
 
-function isMinFile(fileName) {
-	const parsedPath = path.parse(fileName);
-	const name = parsedPath.name.toLowerCase();
-	return name.includes('min');
-}
-
+// creating and writing into min file
 async function writeFile(outputPath, content) {
-	return new Promise((resolve, reject) => {
-		fs.writeFile(outputPath, content, { flag: 'w' }, (err) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve();
-			}
-		});
-	});
+	try {
+		await fsPromises.writeFile(outputPath, content, { flag: 'w' });
+		console.log(`File written to: ${outputPath}`);
+	} catch (error) {
+		console.error(`Error writing file: ${error.message}`);
+		throw error;
+	}
 }
 
+// for creating and editing in a seperate min folder
 function getOutputFilePath(parentPath, fileName, extension, enableSeparateFolder) {
-	const outputPathWithoutExtension = path.join(parentPath, `${path.parse(fileName).name}` + (!changeRootName ? `.min` : ""));
-	console.log(outputPathWithoutExtension);
+	const baseName = path.parse(fileName).name;
+
 	if (enableSeparateFolder) {
 		const minFolder = path.join(parentPath, 'min');
 		if (!fs.existsSync(minFolder)) {
 			fs.mkdirSync(minFolder);
 		}
-		return path.join(minFolder, `${path.parse(fileName).name}` + (!changeRootName ? `.min` : "") + `${extension}`);
+		return path.join(minFolder, `${baseName}.min${extension}`);
 	} else {
-		return `${outputPathWithoutExtension}${extension}`;
+		return path.join(parentPath, `${baseName}.min${extension}`);
 	}
 }
 
